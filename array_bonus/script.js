65f7975c7ff83f383c6639548457b6c901c7ceca
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-12 16:17:22 +0100

    trying to start the array_bonus

---
+++ array_bonus/script.js
@@ -0,0 +1,10 @@
+let majorArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let minorArray = [1, 2, 3, 4, 5, 6,];
+
+for (let i = 0; i < majorArray.length; i++){
+    console.log(i, 'majorarray');    
+}
+
+for (let k = 0; k < minorArray.length; k++){
+    console.log(k, 'minorarray');    
+}
