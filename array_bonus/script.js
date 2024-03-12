7d3fd9cc8f9e6b5fd63be4842cebcad1c2eaa33a
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-12 16:40:55 +0100

    trying other ways to achieve the solution

--- array_bonus/script.js
+++ array_bonus/script.js
@@ -1,10 +1,23 @@
 let majorArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let minorArray = [1, 2, 3, 4, 5, 6,];
+let minorArray = [1, 2, 3, 4, 5, 6];
 
-for (let i = 0; i < majorArray.length; i++){
-    console.log(i, 'majorarray');    
-}
+// for (let i = 0; i < majorArray.length; i++){
+//     // console.log(i, 'majorarray');
+//     let big = majorArray[i];
+//     console.log(big)
+// }
 
-for (let k = 0; k < minorArray.length; k++){
-    console.log(k, 'minorarray');    
+// for (let k = 0; k < minorArray.length; k++){
+//     // console.log(k, 'minorarray');
+//     let small = minorArray[k];
+//     console.log(small)
+// }
+let big = majorArray.length;
+let small = minorArray.length;
+console.log( big, small);
+if (big-small != 0){
+    let size = big-small;
+    console.log(size)
+    for (i = 0; i<= size, i++)
+    
 }
