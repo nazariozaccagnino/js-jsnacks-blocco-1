5099041ce538941913ad7eb193988f7ed640e229
nazariozaccagnino <nazario.zaccagnino@gmail.com>
2024-03-12 16:48:26 +0100

    array_bonus completed

--- array_bonus/script.js
+++ array_bonus/script.js
@@ -1,23 +1,18 @@
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 let majorArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let minorArray = [1, 2, 3, 4, 5, 6];
 
-// for (let i = 0; i < majorArray.length; i++){
-//     // console.log(i, 'majorarray');
-//     let big = majorArray[i];
-//     console.log(big)
-// }
-
-// for (let k = 0; k < minorArray.length; k++){
-//     // console.log(k, 'minorarray');
-//     let small = minorArray[k];
-//     console.log(small)
-// }
 let big = majorArray.length;
 let small = minorArray.length;
 console.log( big, small);
 if (big-small != 0){
     let size = big-small;
     console.log(size)
-    for (i = 0; i<= size, i++)
-    
+    for (i = 0; i< size; i++){
+        minorArray.push(getRndInteger(90, 100));
+    }
+    console.log(minorArray, 'array completato con numeri mancanti random')    
 }
